fca85603b75935a894b578d86e7a9b0ac3571bb9
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-04-30 01:54:35 +0600

    productbykeys

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-require('dotenv').config()
-
+require("dotenv").config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lion9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -19,39 +17,47 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
-
 async function run() {
   try {
     await client.connect();
     const productCollection = client.db("eshop").collection("products");
-    console.log("Connected")
-    app.get("/products", async(req, res)=>{
-      const page = parseInt(req.query.page)
-      const size = parseInt(req.query.size)
-      console.log('query', req.query)
-      const query ={}
+    console.log("Connected");
+    app.get("/products", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log("query", req.query);
+      const query = {};
       const cursor = productCollection.find(query);
-      let products
-      if(page || size)
-      {
-        products = await cursor.skip(page*size).limit(size).toArray();
-      }
-      else
-      {
+      let products;
+      if (page || size) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
         products = await cursor.toArray();
       }
-      res.send(products)
+      res.send(products);
       // console.log(products)
-    })
+    });
 
-    app.get("/productcount", async(req,res)=>{
-      const query = {}
-      const cursor = productCollection.find(query)
-      const count = await collection.estimatedDocumentCount();
+    app.get("/productcount", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const count = await productCollection.countDocuments();
       // res.json(count)
-      res.json({count})
-    })
+      res.json({ count });
+    });
+
+    app.post("/productByKeys", async (req, res) => {
+      const keys = req.body;
+      const ids = keys.map((id) => ObjectId(id));
+      const query = { _id: { $in: ids } };
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      console.log(keys);
+      res.send(products);
+    });
   } finally {
   }
 }
@@ -61,7 +67,6 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Running");
 });
-
 
 app.listen(port, () => {
   console.log("Server running", port);
